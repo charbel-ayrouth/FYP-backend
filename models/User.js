c3703d4e8c56c6a36102647e8890b640d5846671
charbel ayrouth <ayrouthcharbel@gmail.com>
2023-03-29 14:41:50 +0300

    .

--- models/User.js
+++ models/User.js
@@ -1,52 +1,54 @@
 import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    // required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['Student', 'Supervisor', 'Admin'],
-    default: 'Student',
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  topics: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'TopicOfInterest',
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
     },
-  ],
-  domains: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'DomainOfApplication',
+    email: {
+      type: String,
+      required: true,
     },
-  ],
-  connections: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+    password: {
+      type: String,
+      required: true,
     },
-  ],
-  connectionRequest: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'User',
+    role: {
+      type: String,
+      enum: ['Student', 'Supervisor', 'Admin'],
+      default: 'Student',
     },
-  ],
-})
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    topics: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'TopicOfInterest',
+      },
+    ],
+    domains: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'DomainOfApplication',
+      },
+    ],
+    connections: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    connectionRequest: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+  { timestamps: true }
+)
 
 const User = mongoose.model('User', userSchema)
 
